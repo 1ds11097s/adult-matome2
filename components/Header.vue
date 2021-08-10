83e915ede38c9019d4d6c2f35bd564ade2dce648
大坪 裕二 <1ds11097s@gmail.com>
2021-08-10 23:51:02 +0900

    リンク追加

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="text-center">
-    <h1>ガチ痙攣イキする動画だけを集めてAVレビュー</h1>  
+    <h1><a href="https://keirenavmatome.xyz/" style="text-decoration:none;color:black">ガチ痙攣イキする動画だけを集めてAVレビュー</a></h1>  
     <span>最強の痙攣絶頂動画をひたすら紹介！</span>
     <br />
     <span>痙攣絶頂、ポルチオ開発、Gスポット刺激などでビクンビクンイキまくる動画を厳選しています、あなたの探していた動画が必ず見つかります！</span>
